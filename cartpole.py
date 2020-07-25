b285840619cd781980456bfa473932af9dad045b
simondlevy <simon.d.levy@gmail.com>
2020-07-25 17:49:45 +0000

    Begin saving work

--- cartpole.py
+++ cartpole.py
@@ -30,5 +30,5 @@ if __name__ == '__main__':
 
     best = ga.run(10, max_fitness=2000)
 
-    print('Got reward %.3f in %d steps' % problem.run_episode(best, render=True))
+    #print('Got reward %.3f in %d steps' % problem.run_episode(best, render=True))
 
--- pendulum.py
+++ pendulum.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
 
     problem = NefPendulum()
 
-    ga = Elitist(problem, 2048)
+    ga = Elitist(problem, 2048, save_name='pendulum')
 
     best = ga.run(80, max_fitness=-1500)
 
-    print('Got reward %.3f in %d steps' % problem.run_episode(best, render=True))
+    #print('Got reward %.3f in %d steps' % problem.run_episode(best, render=True))
 
