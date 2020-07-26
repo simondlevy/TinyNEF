409074a3e40528fd964079cf5e1f4370c08b3e0e
simondlevy <simon.d.levy@gmail.com>
2020-07-26 18:22:06 -0400

    Factored GymProblem to sueap.gym

--- gym/lib/__init__.py
+++ gym/lib/__init__.py
@@ -8,22 +8,18 @@ MIT License
 
 import gym
 import numpy as np
+from sueap.gym import GymProblem
 
-class NefGym:
+class NefGym(GymProblem):
 
     def __init__(self, env, neurons, seed):
 
-        # Seed random-number generator if indicated
-        if seed is not None:
-            np.random.seed(seed)
-
-        # Get observation size from environment
-        obs_size = gym.make(env).observation_space.shape[0]
+        GymProblem.__init__(self, env, seed)
 
         # Encoder
         self.alpha = np.random.uniform(0, 100, neurons) # tuning parameter alpha
         self.b = np.random.uniform(-20,+20, neurons)    # tuning parameter b
-        self.e = np.random.uniform(-1, +1, (obs_size,neurons)) # encoder weights
+        self.e = np.random.uniform(-1, +1, (self.obs_size,neurons)) # encoder weights
 
         self.seed = seed
         self.env = env
@@ -57,42 +53,6 @@ class NefGym:
 
         return self.alpha, self.b,  self.e, params
 
-    def run_episode(self, params, render=False):
-
-        # Build env
-        env = gym.make(self.env)
-
-        if self.seed is not None:
-            env.seed(self.seed)
-
-        obs = env.reset()
-
-        episode_reward, episode_steps = 0,0
-
-        # Simulation loop
-        while True:
-
-            action = self._get_action(params, obs)
-
-            # Optional render of environment
-            if render:
-                env.render()
-
-            # Do environment step
-            obs, reward, done, _ = env.step(action)
-
-            episode_reward += reward
-            episode_steps += 1
-
-            # Episode end
-            if done:
-                break
-
-        # Cleanup
-        env.close()
-
-        return episode_reward, episode_steps
-
     def test(self, net):
 
         self.alpha = net[0]
@@ -101,7 +61,7 @@ class NefGym:
 
         return self.run_episode(net[3], render=True)
 
-    def _get_action(self, params, obs):
+    def get_action(self, params, obs):
 
         return self.activate(np.dot(self._curve(obs), params))
 
